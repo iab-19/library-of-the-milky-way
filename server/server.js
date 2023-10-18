3aea9bb9bb0c20f9ac70b049624fda1897edf469
Ibrahima <iab.jr10@gmail.com>
2023-10-18 04:42:34 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,19 +1,22 @@
-const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
-
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+
+const { authMiddleware } = require('./utils/auth');
 const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
 });
 
-const app = express();
-const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -22,17 +25,22 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+// app.use(routes);
 
-app.use(routes);
-const startApolloServer = async (typeDefs, resolvers) => {
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+const startApolloServer = async () => {
   await server.start();
   server.applyMiddleware({app});
+
   db.once('open', () => {
     app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`)
-    console.log(`graphql playground url: localhost:${PORT}${server.graphqlPath}`)
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`graphql playground url: http://localhost:${PORT}${server.graphqlPath}`);
+  })
   });
-  });
-}
+};
 
-startApolloServer(typeDefs, resolvers);
+startApolloServer();
