b6bb675f6815c0b95287436a296deefe7711a8c4
Ibrahima <iab.jr10@gmail.com>
2023-10-18 05:58:24 -0400

    Add mutation to delete a book from a user's booklist

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,24 @@
-const { signToken } = require('../utils/auth');
-const { User } = require('../models/');
 const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('../models/');
+const { signToken } = require('../utils/auth');
+
+
 
 const resolvers = {
     Query: {
+        user: async (parent, { username }) => {
+            return User.findOne({ username }).populate('books');
+        },
+
+        books: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Book.find(params);
+        },
+        book: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Book.find(params);
+        },
+
         me: async (parent, args, context) => {
             if (context.user) {
                 const user = await User.findOne({_id: context.user._id});
@@ -11,7 +26,46 @@ const resolvers = {
             }
 
             throw new AuthenticationError('User not found');
-        }
+        },
+    },
+
+    Mutation: {
+        getSingleUser: async (parent) => {
+
+        },
+
+        // create new user
+        createUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+
+        },
+
+        // login
+        login: async (parent) => {
+
+        },
+
+        // save a book
+        saveBook: async (parent) => {
+
+        },
+
+        // delete a book
+        deleteBook: async (parent, { bookId }) => {
+            if (context.user) {
+                const book = await Book.findOneAndDelete({ _id: bookId });
+
+
+            await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { books: book._id }}
+            );
+
+            return book;
+            }
+            throw new AuthenticationError('You need to be logged in to delete a book');
+        },
     }
 }
 
