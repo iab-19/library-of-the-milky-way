514c5f6b7de6cbcf10e55642f372976f8d0ec7ce
Ibrahima <iab.jr10@gmail.com>
2023-10-31 00:27:21 -0400

    Deploy to Heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/milkyWay');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/milky-way');
 
 module.exports = mongoose.connection;
